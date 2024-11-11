5ddf344cfeadcf5c361932b0b1a7c7ea1f801fb5
Konrad Marciniak <marciniakkonrad@outlook.com>
2024-11-11 18:50:42 +0100

    Pause all players, play 'last playing' script

---
+++ .scripts/play_pause_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+playing=$(playerctl -a status | grep -c Playing)
+if [ $playing -eq 0 ]; then
+	playerctl play --player=playerctld
+else
+	last_player=$(playerctl status -a --format "{{status}}{{playerName}}" | grep Playing | sed 's/Playing//' | head -n 1)
+	playerctl pause --ignore-player=$last_player --all-players
+	playerctl pause --player=$last_player
+fi
