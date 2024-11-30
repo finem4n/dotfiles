fe4c387b33ec0388d18ae9134637357acecf14e0
Konrad Marciniak <marciniakkonrad@outlook.com>
2024-11-30 20:15:33 +0100

    feat: updated path with ZSH var in update_everything.sh

--- .scripts/update_everything.sh
+++ .scripts/update_everything.sh
@@ -6,7 +6,7 @@
 function omz_custom_plugins {
 	echo "Git pulling custom Oh My Zsh plugins"
 	echo ""
-	for i in $HOME/.oh-my-zsh/custom/plugins/*/.git; do
+	for i in $ZSH/custom/plugins/*/.git; do # FIX add if statements for existance of files xD, and if they dont echo "No custom plugins installed" or sth
 		working_dir_path="$(dirname $i)"
 		working_dir=$(echo $working_dir_path | rev | cut -d'/' -f1 | rev)
 		echo "	pulling $working_dir in $working_dir_path"
