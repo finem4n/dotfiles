a2e15091de517c057e8c89cf3b749e3333d5797a
Konrad Marciniak <marciniakkonrad@outlook.com>
2025-02-19 22:54:08 +0100

    add mako config

--- .scripts/mako_menu.sh
+++ .scripts/mako_menu.sh
@@ -4,21 +4,23 @@ source $HOME/.scripts/bemenu-opts.sh
 
 function main () {
     mode="("$(makoctl mode)"):"
-    op=$( echo -e "󱈸 Actions\n Dismiss all\n Restore\n󱨩 Mode switch" | bemenu -i -l 4 --prompt "${mode}" --fn 'JetBrainsMono Nerd Font 20' --line-height 55 --ch 40 --cw 3 | awk '{print tolower($2)}' )
+    op=$( echo -e "󱈸 Actions\n Restore\n Dismiss all\n󱨩 Mode switch" | bemenu -i -I $1 -l 4 --prompt "${mode}" --fn 'JetBrainsMono Nerd Font 20' --line-height 55 --ch 40 --cw 3 | awk '{print tolower($2)}' )
 
     case $op in 
         actions)
             makoctl menu bemenu -l 4 --prompt 'Choose Action: ' --fn 'JetBrainsMono Nerd Font 20' --line-height 55 --ch 40 --cw 3 
-            main
-            ;;
-        dismiss)
-            makoctl dismiss -a
+            main 0
             ;;
         restore)
             makoctl restore
+            main 1
+            ;;
+        dismiss)
+            makoctl dismiss -a
+            main 2
             ;;
         mode)
-            mode_switch        
+            mode_switch
             ;;
     esac
 }
@@ -28,22 +30,22 @@ function mode_switch () {
     op=$( echo -e "󰂚 Default\n󰂛 DND\n󰂞 Unmuted" | bemenu -i -l 3 --prompt "${mode}" --fn 'JetBrainsMono Nerd Font 20' --line-height 55 --ch 40 --cw 3 | awk '{print tolower($2)}' )
 
     case $op in 
-            default)
-                makoctl mode -s default && pkill -RTMIN+11 waybar
-                echo $op
-                    ;;
-            dnd)
-                makoctl mode -s dnd && pkill -RTMIN+11 waybar
-                echo $op
-                    ;;
-            unmuted)
-                makoctl mode -s unmuted && pkill -RTMIN+11 waybar
-                echo $op
+        default)
+            makoctl mode -s default && pkill -RTMIN+11 waybar
+            ;;
+        dnd)
+            makoctl mode -s dnd && pkill -RTMIN+11 waybar
+            ;;
+        unmuted)
+            makoctl mode -s unmuted && pkill -RTMIN+11 waybar
+            ;;
     esac
 }
 
 if pgrep -x bemenu; then
     killall bemenu
+elif [ "$1" == "--actions" ]; then
+    makoctl menu bemenu -l 4 --prompt 'Choose Action: ' --fn 'JetBrainsMono Nerd Font 20' --line-height 55 --ch 40 --cw 3 
 else
-    main
+    main 0
 fi
