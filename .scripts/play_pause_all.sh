2bad3a96afccefefe112fa63db65a82b4070f185
Konrad Marciniak <marciniakkonrad@outlook.com>
2025-02-20 15:58:16 +0100

    clean up

--- .scripts/play_pause_all.sh
+++ .scripts/play_pause_all.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 playing=$(playerctl -a status | grep -c Playing)
-if [ $playing -eq 0 ]; then
-	playerctl play --player=playerctld
-else
+
+function pause_and_remember () {
 	last_player=$(playerctl status -a --format "{{status}}{{playerName}}" | grep Playing | sed 's/Playing//' | head -n 1)
 	playerctl pause --ignore-player=$last_player --all-players
 	playerctl pause --player=$last_player
+}
+
+if [ "$#" -eq 0 ]; then
+	if [ $playing -eq 0 ]; then
+		playerctl play --player=playerctld
+	else
+		pause_and_remember
+	fi
+elif [ "$1" == "--pnr" ]; then
+	pause_and_remember
 fi
